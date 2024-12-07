12ab1e20ba011b80d6ffb91e9381a00964ce9239
JYJ <joy6924@naver.com>
2024-12-07 17:07:54 +0900

    change fib function to sum function

--- program.c
+++ program.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-int fib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return 42; // 임시 반환값
+int sum(int n)
+{
+  if (n == 0) return 0;
+  return n + sum(n-1);
 }
 
 int main()
